baa78e68686d64bfda97618ed6a4e4f2511d073c
hub-naveen <nobytes7@gmail.com>
2026-02-24 18:06:55 +0530

    Add HTML structure and CSS styles for Healthy Lifestyle Blog; include JavaScript data types and notes

---
+++ Day_4/task/datatype.js
@@ -0,0 +1,16 @@
+var str = "Hello World"
+var nmum =123
+var bool = true
+var undefvar = undefined
+var Bigint = 324578790223456789;
+var sym = Symbol("gfgdsxfvbnk")
+var obj = {DF : "TGBKJH", age : 40 };
+
+console.log(str);
+console.log(nmum);
+console.log(bool);
+console.log(undefvar);
+console.log(Bigint);
+console.log(sym);
+console.log(obj);
+
